908b4919b348516fb0f5da4491e3444355bbe482
Henriette Röger <A200158358@t000e97013.fritz.box>
2024-04-17 11:16:13 +0200

    added new trained model

--- fivesafe/object_detection/train_yolov8.py
+++ fivesafe/object_detection/train_yolov8.py
@@ -5,7 +5,8 @@ model = YOLO('yolov8s.pt')
  
 # Training.
 results = model.train(
-   data='conf/dataset.yaml',
+   #data='conf/dataset.yaml',
+   data='/Users/A200158358/Desktop/5 safe/girlsday/git_girlsday/girlsday_5safe/conf/train.yaml',
    imgsz=640,
    epochs=8,
    name='finetuned'
